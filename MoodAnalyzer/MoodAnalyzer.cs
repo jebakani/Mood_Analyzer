249f453653a9655006af8c7056b6de78d82560cc
Jebakani <vjishwarya@gmail.com>
2021-07-15 19:18:53 +0530

    [JEBAKANI] Executing program to analyse mood from message

---
+++ MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzers
+{
+    class MoodAnalyzer
+    {
+        public string AnalyseMood(string message)
+        {
+            message = message.ToLower();
+            if(message.Contains("sad"))
+            {
+                return "sad";
+            }
+            else
+            {
+                return "happy";
+            }
+        }
+    }
+}
